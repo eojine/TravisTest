6812b2526d0e0307ac51d2b9a7da8ac33b975545
eojin <eozinee@gmail.com>
2020-10-30 15:36:06 +0900

    init

---
+++ TravisTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisTest
+//
+//  Created by 양어진 on 2020/10/30.
+//  Copyright © 2020 eojine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
